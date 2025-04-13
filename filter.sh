dd889797ff0a0fd4bad0fe12d95a4dd4d9350ebd
taylorjayoung <tayloryoung@mvmmediasales.com>
2025-04-13 16:39:05 -0400

    Add filter script for removing API keys

--- filter.sh
+++ filter.sh
@@ -1,4 +1,2 @@
-
-    #!/bin/sh
-    sed -i 's/sk-ant-api03-[A-Za-z0-9_-]\{40,\}/YOUR_ANTHROPIC_API_KEY/g' "$@"
-    
+#!/bin/sh
+sed -i 's/sk-ant-api03-[A-Za-z0-9_-]\{40,\}/YOUR_ANTHROPIC_API_KEY/g' "$@"
